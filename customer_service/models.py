d1d9679b9214b617664d9cfa66c3a0a0e4b522ca
Maulana Seto <maulana.seto@ui.ac.id>
2023-10-27 00:08:01 +0700

    Add path for cs app

--- customer_service/models.py
+++ customer_service/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from book.models import Book
+from beranda.models import User
 
-# Create your models here.
+class BookReport(models.Model):
+    STATUS = [("UNDONE", "Undone"), ("SEMI-DONE", "Semi-done"), ("DONE", "Done")]
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    losts = models.ManyToManyField(Book, related_name='lost_books', blank=True)
+    brokens = models.ManyToManyField(Book, related_name='broken_books', blank=True)
+    report_date = models.DateField(auto_now_add=True)
+    status = models.CharField(choices=STATUS)
+    message = models.TextField(blank=True)
+
+class RoomReport(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    new_room = None
+    cancel = models.BooleanField(default=False)
+    report_date = models.DateField(auto_now_add=True)
+    status = models.BooleanField()
+
+class Complaint(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    complaint = models.TextField()
+    report_date = models.DateField(auto_now_add=True)
--- customer_service/views.py
+++ customer_service/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+import datetime
+from book.models import Book
+from beranda.models import User
+from customer_service.models import BookReport, RoomReport, Complaint
+from django.urls import reverse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.core import serializers
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf import csrf_exempt
 
-# Create your views here.
+@login_required(login_url='/login')
+def show_customer_service(request):
+    reports = BookReport.objects.filter(user=request.user) + RoomReport.objects.filter(user=request.user)
+    return render(request, "customer_service.html", reports)
+
+def show_customer_servicer(request):
+    reports = BookReport.objects.all() + RoomReport.objects.all()
+
+def get_product_json(request):
+    books = Book.objects.filter(user=request.user)
+    return HttpResponse(serializers.serialize('json', books))
+
+@csrf_exempt
+def add_lost(request, id):
+    if request.method == 'POST':
+        user = request.user
+        book_report = BookReport.objects.create(user=user)
+        
+        book_report.losts.set(Book.objects.filter(id__in=request.POST.getlist('status')))
+        book_report.brokens.set(Book.objects.filter(id__in=request.POST.getlist('status')))
+
+    return HttpResponseNotFound()
