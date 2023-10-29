d5a51c13c3daa336ecd16cd81c4be734c6153aeb
rhaken <rhakenss@gmail.com>
2023-10-29 16:02:04 +0700

    update sistem_manajemen

--- sistem_manajemen/urls.py
+++ sistem_manajemen/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from sistem_manajemen.views import show_sistem, get_ruangan_json, add_ruangan_ajax, show_sistem_ruangan, show_sistem_buku, get_buku_json
+from sistem_manajemen.views import show_sistem, get_ruangan_json, add_ruangan_ajax, show_sistem_ruangan, show_sistem_buku, get_buku_json, ganti_status_ketersediaan
 
 app_name = 'sistem_manajemen'
 
@@ -9,5 +9,6 @@ urlpatterns = [
     path('create-ruangan-ajax/', add_ruangan_ajax, name='add_ruangan_ajax'),
     path('sistem_ruangan', show_sistem_ruangan, name="sistem_ruangan"),
     path('sistem_buku', show_sistem_buku, name="sistem_buku"),
-    path('get_buku_json', get_buku_json, name="get_buku_json")
+    path('get_buku_json/<str:id>', get_buku_json, name="get_buku_json"),
+    path('edit_ketersediaan/<int:id>', ganti_status_ketersediaan, name='edit_ketersediaan'),
 ]
